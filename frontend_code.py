a573415f66fc9896edd064a611adbade62da103f
NimanshJ <jainnimansh@gmail.com>
2022-10-25 17:26:18 +0530

    navbar and backspace fixed

--- frontend_code.py
+++ frontend_code.py
@@ -4,6 +4,7 @@ import sys
 import time
 import os
 
+#os.chdir(r"C:\Users\Prisha\Desktop\Prisha\Python\TS\Velocity Sudoku solver\Velocity_prog-main")
 
 root = tkinter.Tk()
 root.withdraw()
@@ -18,8 +19,11 @@ pygame.display.set_caption('Sudoku Solver')
 
 base_font = pygame.font.Font("Outfit.ttf", 25)
 base_font1 = pygame.font.Font("Outfit-SemiBold.ttf", 20)
+navbar_font = pygame.font.Font("dosis.ttf", 42)
 user_text = ''
 
+navbar_rect = pygame.Rect(0, 0, 600, 42)
+color_nav = (140, 140, 140)
 
 input_rect = pygame.Rect(35, 550, 200, 27)
 color_active = pygame.Color((190, 190, 190))
@@ -41,7 +45,7 @@ array = ""
 
 sudokuempty = pygame.image.load("empty_sudoku_board.png")
 sudokuempty = pygame.transform.scale(sudokuempty, [450, 450])
-
+#text_width, text_height = base_font.size(user_text)
 
 while True:
     for event in pygame.event.get():
@@ -74,6 +78,7 @@ while True:
                 elif ctrl:
                     if event.key == pygame.K_v:
                         user_text += root.clipboard_get()
+                        copy = True
                 else:
                     user_text += event.unicode
         if event.type == pygame.KEYUP:
@@ -99,31 +104,39 @@ while True:
             time.sleep(0.13)
         else:
             time.sleep(0.03)
-        user_text = user_text[:-1]
+        user_text = temp[:-1]
+        temp = temp[:-1]
         backsp_c += 1
 
     input_rect.centerx = screen_width/2
-    text_width, text_height = base_font.size(user_text) 
+    
     if len(user_text) > 1:
         if copy:
             array = user_text
+            temp = array
             copy = False
-    screen.blit(sudokuempty, [75, 50])
+    text_width, text_height = base_font.size(user_text)
+    screen.blit(sudokuempty, [75, 70])
     for i in range(100000):
         if text_width > 440:
             user_text = user_text[:-1]
-        #     print(user_text)
-        else:break
+        else:
+            text_surface = base_font.render(user_text, True, (1, 1, 1))
+
+            break
         break
-    
-    
+   
     pygame.draw.rect(screen, color, input_rect, width=0, border_radius=20, border_top_left_radius=40, border_top_right_radius=40, border_bottom_left_radius=40, border_bottom_right_radius=40) #input button  
-    text_surface = base_font.render(user_text, True, (1, 1, 1))
     screen.blit(text_surface, (input_rect.x+5, input_rect.y))
- 
+
     pygame.draw.rect(screen, color_b, submit_rect, border_radius=20)
     text_surface_b = base_font1.render("Submit", True, (1, 1, 1))
     screen.blit(text_surface_b, (submit_rect.x+7, submit_rect.y+2))
+
+    pygame.draw.rect(screen, color_nav, navbar_rect, border_bottom_left_radius=5, border_bottom_right_radius=5)
+    text_navbar = navbar_font.render("Sudoku Solver", True, (140, 140, 200))
+    screen.blit(text_navbar, (200, -5))
+
 
     input_rect.w = 530
 
